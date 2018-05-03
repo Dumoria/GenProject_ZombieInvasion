2b950169f625f568a4902aa5d7b36695cec9c53e
Cloud <benjamin.thomas@heig-vd.ch>
2018-05-03 16:13:33 +0200

    Debut d'implementation du cote client/connexion serveur

--- test/core/src/net/client/Client.java
+++ test/core/src/net/client/Client.java
@@ -1,18 +1,75 @@
 package net.client;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Logger;
 
 public class Client {
 
-    private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+    //----------------Server connection member-------------------
+    private static final Logger LOG = Logger.getLogger(Client.class.getName());
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
     private boolean connected;
+    private static Gson gson;
 
 
+    //---------------Constructor---------------------------------
+    public Client() {
+        this.connected = false;
 
+        //Prépare le moteur gson
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+
+    //---------------Methods-------------------------------------
+
+    public void auth(String username, String password) throws IOException{
+        if(!connected)
+            connect(Protocol.DEFAULT_ADDRESS, Protocol.DEFAULT_PORT);
+        gson.toJson(new UserJson(username, password), out);
+        out.flush();
+
+    }
+
+    public void connect(String server, int port) throws IOException {
+        socket = new Socket(server, port);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream());
+        connected = true;
+    }
+
+    public void disconnect() throws IOException {
+
+        if(socket != null) {
+            socket.close();
+        }
+        if(in != null) {
+            in.close();
+        }
+        if(out != null) {
+            out.close();
+        }
+
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected && (socket != null);
+
+    }
+
+    public String readServer() throws IOException{
+        return in.readLine();
+    }
+
+    public void writeServer(String str){
+        out.write(str + '\n');
+        out.flush();
+    }
 }
---
+++ test/core/src/net/client/UserJson.java
@@ -0,0 +1,12 @@
+package net.client;
+
+public class UserJson {
+
+    private String username;
+    private String password;
+
+    public UserJson(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
