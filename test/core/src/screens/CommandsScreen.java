84d77d5637e283ef1eb1beb14dc08b239ba43f7e
Cloud <benjamin.thomas@heig-vd.ch>
2018-05-11 14:46:42 +0200

    debut des commands

--- test/core/src/screens/CommandsScreen.java
+++ test/core/src/screens/CommandsScreen.java
@@ -15,43 +15,45 @@ public class CommandsScreen extends GenericScreen {
         super(game);
 
         //-----------save button-----------
-        save = new TextButton("Start", skin);
+        save = new TextButton("Quit", skin);
         save.setPosition(50,20);
         save.setSize(90, 30);
         save.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                //save the command in a new file
-            }
-        });
-
-        stage.addActor(save);
-
-        //-----------Commands button-----------
-        quit = new TextButton("Commands", skin);
-        quit.setPosition(500,60);
-        quit.setSize(90, 30);
-        quit.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 game.setScreen(new StartScreen(game));
             }
         });
 
+        stage.addActor(save);
+
+        //-----------Commands button-----------
+        quit = new TextButton("Save", skin);
+        quit.setPosition(500,60);
+        quit.setSize(90, 30);
+        quit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                //save the command in a new file (commandsNew)
+            }
+        });
+
         stage.addActor(quit);
 
         //-----------reinit button-----------
-        reinit = new TextButton("High scores", skin);
+        reinit = new TextButton("Reinit", skin);
         reinit.setPosition(500,20);
         reinit.setSize(90, 30);
         reinit.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                //reload config init
+                //reload commandsOrigin
             }
         });
 
         stage.addActor(reinit);
+
+
 
     }
 
