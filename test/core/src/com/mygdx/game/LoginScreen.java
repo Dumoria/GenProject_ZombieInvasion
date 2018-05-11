882b31e6fcf9ef3ecb63867f2231b97705dfaaef
Cloud <benjamin.thomas@heig-vd.ch>
2018-05-11 13:04:41 +0200

    background

--- test/core/src/com/mygdx/game/LoginScreen.java
+++ test/core/src/com/mygdx/game/LoginScreen.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
@@ -25,6 +28,9 @@ public class LoginScreen implements Screen {
     private TextField username;
     private TextField password;
 
+    SpriteBatch batch;
+    Texture img;
+
     public TextField getUsername() {
         return username;
     }
@@ -34,10 +40,15 @@ public class LoginScreen implements Screen {
     }
 
     public LoginScreen(Game game, final Client client) {
+        batch = new SpriteBatch();
+        img = new Texture("core/src/resources/LoginScreen.jpg");
+
         this.game = game;
         stage = new Stage();
+
         Gdx.input.setInputProcessor(stage);
         Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
 
         connect = new TextButton("Connect", skin);
         connect.setPosition(300,150);
@@ -91,6 +102,10 @@ public class LoginScreen implements Screen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.begin();
+		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.end();
+
         stage.act(delta);
         stage.draw();
     }
@@ -117,6 +132,7 @@ public class LoginScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        img.dispose();
     }
 }
